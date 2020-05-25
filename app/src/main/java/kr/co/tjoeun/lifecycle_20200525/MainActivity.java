03bc4066e1d66cd23addb87f8debaecbebfd3df2
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-25 15:41:37 +0900

    onBackPressed 오버라이딩  - 1초 이내에 다시 눌러야 진짜 종료되도록

--- app/src/main/java/kr/co/tjoeun/lifecycle_20200525/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200525/MainActivity.java
@@ -7,12 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 import kr.co.tjoeun.lifecycle_20200525.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
+
+//    최근에 백버튼을 누른 시간 기록
+    long lastBackTime = 0;
 
 
     @Override
@@ -53,5 +59,24 @@ public class MainActivity extends BaseActivity {
     protected void onPause() {
         super.onPause();
         Log.d("생명주기확인", "onPause 실행");
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+ㅐ
+        long now = System.currentTimeMillis();
+
+//        현재 시간 - 지난번백버튼누른시간이 1초이상? (너무 오랜만에 누름)
+        if(now - lastBackTime> 1000) {
+            Toast.makeText(mContext, "한번 더 취소키을 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            finish();
+        }
+
+//        언제 눌렀는지 기록 갱신
+        lastBackTime = now;
+
     }
 }
