c1e039920cd15ff94d48c4ebd88fc7daee4560c4
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-25 14:50:02 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20200525/MainActivity.java
@@ -0,0 +1,29 @@
+package kr.co.tjoeun.lifecycle_20200525;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupEvents();
+        setValues();
+
+        Log.d("생명주기 확인", "onCreate가 실행된다");
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
